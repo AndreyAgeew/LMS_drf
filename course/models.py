01c8105fc6e351afff5e41fc3475d716c26d3cf1
Andrey Ageew <faceofdeviler@gmail.com>
2023-09-27 18:41:38 +0300

    changed preview: null=True

--- course/models.py
+++ course/models.py
@@ -3,7 +3,7 @@ from django.db import models
 
 class Course(models.Model):
     title = models.CharField(max_length=50, verbose_name='название')
-    preview = models.ImageField(upload_to='course/', verbose_name='превью')
+    preview = models.ImageField(upload_to='course/', verbose_name='превью', null=True)
     description = models.TextField(verbose_name='описание')
 
     def __str__(self):
@@ -13,7 +13,7 @@ class Course(models.Model):
 class Lesson(models.Model):
     title = models.CharField(max_length=150, verbose_name='название')
     description = models.TextField(verbose_name='описание')
-    preview = models.ImageField(upload_to='lesson/', verbose_name='превью')
+    preview = models.ImageField(upload_to='lesson/', verbose_name='превью', null=True)
     url = models.URLField(verbose_name='ссылка на видео')
 
     course = models.ForeignKey(Course, on_delete=models.CASCADE, verbose_name="курс")
