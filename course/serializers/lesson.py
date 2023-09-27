5ef1511ebafb8298f4e5d9e6b6a2bde1127b7347
Andrey Ageew <faceofdeviler@gmail.com>
2023-09-27 17:40:56 +0300

    raname

--- course/serializers/lesson.py
+++ course/serializers/lesson.py
@@ -3,7 +3,7 @@ from rest_framework import serializers
 from course.models import Lesson
 
 
-class CourseSerializer(serializers.ModelSerializer):
+class LessonSerializer(serializers.ModelSerializer):
     class Meta:
         model = Lesson
         fields = '__all__'
