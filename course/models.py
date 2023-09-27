a3ab1a69b458cf0c2b23fb2c18ce7d5d08b56bcb
Andrey Ageew <faceofdeviler@gmail.com>
2023-09-27 17:25:51 +0300

    add model Course

--- course/models.py
+++ course/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+
+class Course(models.Model):
+    name = models.CharField(max_length=50, verbose_name='название')
+    preview = models.ImageField(upload_to='course/', verbose_name='превью')
+    description = models.TextField(verbose_name='описание')
+
+    def __str__(self):
+        return f'{self.name}'
+
