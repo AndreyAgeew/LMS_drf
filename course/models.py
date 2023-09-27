f519f7d5a82babc4edf5eef0448027a60c007cad
Andrey Ageew <faceofdeviler@gmail.com>
2023-09-27 17:29:26 +0300

    changed name to title

--- course/models.py
+++ course/models.py
@@ -2,10 +2,11 @@ from django.db import models
 
 
 class Course(models.Model):
-    name = models.CharField(max_length=50, verbose_name='название')
+    title = models.CharField(max_length=50, verbose_name='название')
     preview = models.ImageField(upload_to='course/', verbose_name='превью')
     description = models.TextField(verbose_name='описание')
 
     def __str__(self):
-        return f'{self.name}'
+        return f'{self.title}'
+
 
