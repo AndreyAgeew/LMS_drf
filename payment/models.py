df79d33cd962f6d4e62c44172181150a68cc7e19
Andrey Ageew <faceofdeviler@gmail.com>
2023-09-29 21:49:09 +0300

    add model Payment

--- payment/models.py
+++ payment/models.py
@@ -1,3 +1,26 @@
 from django.db import models
+from users.models import User
 
-# Create your models here.
+
+class Payment(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, verbose_name="пользователь")
+    date = models.DateField(verbose_name="дата оплаты")
+    course = models.ForeignKey('course.Course', on_delete=models.CASCADE, null=True, blank=True,
+                               verbose_name="оплаченный курс")
+    lesson = models.ForeignKey('course.Lesson', on_delete=models.CASCADE, null=True, blank=True,
+                               verbose_name="оплаченный урок")
+    amount = models.DecimalField(max_digits=10, decimal_places=2, verbose_name="сумма оплаты")
+
+    PAYMENT_METHOD_CHOICES = [
+        ('cash', 'Наличные'),
+        ('transfer', 'Перевод на счет'),
+    ]
+    payment_method = models.CharField(
+        max_length=10,
+        choices=PAYMENT_METHOD_CHOICES,
+        default='cash',
+        verbose_name="способ оплаты"
+    )
+
+    def __str__(self):
+        return f'{self.user.username} - {self.date} - {self.amount}'
