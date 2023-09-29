43e110a0ee539c7b369db2ca5263573c64ff361b
Andrey Ageew <faceofdeviler@gmail.com>
2023-09-29 21:37:27 +0300

    add get_num_lessons

--- course/serializers/course.py
+++ course/serializers/course.py
@@ -1,9 +1,16 @@
 from rest_framework import serializers
 
 from course.models import Course
+from course.serializers.lesson import LessonSerializer
 
 
 class CourseSerializer(serializers.ModelSerializer):
+    num_lessons = serializers.SerializerMethodField()
+    lessons = LessonSerializer(many=True, read_only=True, source='lesson_set')
+
     class Meta:
         model = Course
         fields = '__all__'
+
+    def get_num_lessons(self, obj):
+        return obj.lesson_set.count()
